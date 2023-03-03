55dae2826b996ebc58ad15ae9dcf0445e6df7e47
JanCTLai <126838445+JanCTLai@users.noreply.github.com>
2023-03-02 22:58:20 -0500

    Update GG618Assignment1.sql

--- GG618Assignment1.sql
+++ GG618Assignment1.sql
@@ -1,6 +1,8 @@
 -- !preview conn=DBI::dbConnect(RSQLite::SQLite())
---exercise2.1.1
-CREATE TABLE ex_2_1_1 
+--exercise2.1.1
+
+CREATE TABLE ex_2_1_1 
+
 AS
 WITH results AS (
 SELECT seq, edge AS gid, cost FROM pgr_dijkstra(
@@ -21,11 +23,13 @@ SELECT
 FROM results
 LEFT JOIN ways
   USING (gid)
-ORDER BY seq;
+ORDER BY seq;
+
 
 
 --Exercise 2.1.2
-CREATE TABLE ex_2_1_2 
+CREATE TABLE ex_2_1_2 
+
 AS
 WITH results AS (
 SELECT seq, edge AS gid, cost As length_m FROM pgr_dijkstra(
@@ -49,7 +53,8 @@ LEFT JOIN ways
 ORDER BY seq;
 
 --Exercise 2.1.3
-CREATE TABLE ex_2_1_3
+CREATE TABLE ex_2_1_3
+
 AS
 WITH results AS (
 SELECT seq, edge AS gid, cost As length_m FROM pgr_dijkstra(
@@ -72,9 +77,12 @@ LEFT JOIN ways
   USING (gid)
 ORDER BY seq;
 
---Exercise 3.1.1
-
-CREATE TABLE ex_3_1_1 
+--Exercise 3.1.1
+
+
+
+CREATE TABLE ex_3_1_1 
+
 AS
 WITH results AS (
 SELECT seq, edge AS gid, cost FROM pgr_dijkstra(
@@ -98,8 +106,9 @@ LEFT JOIN ways
   USING (gid)
 ORDER BY seq;
 
---Exercise_3_1_2
-CREATE TABLE ex_3_1_2 
+--Exercise_3_1_2
+
+CREATE TABLE ex_3_1_2 
 AS
 WITH results AS (
 SELECT seq, edge AS gid, cost FROM pgr_dijkstra(
@@ -124,7 +133,7 @@ LEFT JOIN ways
 ORDER BY seq;
 
 --exercise 3.1.3
-CREATE TABLE ex_3_1_3 
+CREATE TABLE ex_3_1_3 
 AS
 WITH results AS (
 SELECT seq, edge AS gid, cost FROM pgr_dijkstra(
@@ -147,5 +156,17 @@ LEFT JOIN ways
   USING (gid)
 ORDER BY seq;
 
+--QUESTION 4 
+--Write a simple sql query to find the closest node id to a point from a lat and lng value
+--TheCambridge Armoury ('-80.3133476','43.3579909') is chosen as our POI
 
+ALTER TABLE ways_vertices_pgr ADD COLUMN geom geometry(Point, 4326);
+UPDATE ways_vertices_pgr SET geom = ST_SetSRID(ST_MakePoint(ways_vertices_pgr.lon, ways_vertices_pgr.lat), 4326);
+
+SELECT 
+     id, osm_id, geom, lon, lat
+FROM 
+     ways_vertices_pgr
+ORDER BY ways_vertices_pgr.geom <-> ST_SetSRID(ST_MakePoint('-80.3133476','43.3579909')::geometry, 4326)
+LIMIT 1
 
